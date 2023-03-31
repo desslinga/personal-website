1fc7bcbf3278bce09240d71600b63773a8e56419
desslinga <dess.linga@gmail.com>
2023-03-31 00:23:18 -0400

    Adding sample home page image

--- pages/basic-component.tsx
+++ pages/basic-component.tsx
@@ -1,8 +1,12 @@
 
+import Image from 'next/image'
+import splashImage from '../assets/sample_image.png'
+
 export default function BasicComponent() {
   return (
       <div className='big-font'>
         Howdy! Welcome to my personal website!
+        <Image className='main-picture' src={splashImage} />
       </div>
   )
 }
