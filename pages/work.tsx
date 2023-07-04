bb0b739d9c12363202683fb5b0c6d5ff36490559
desslinga <dess.linga@gmail.com>
2023-07-04 00:42:49 -0400

    Adding work gallery images

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,10 +1,49 @@
 
 import { Nanum_Myeongjo } from 'next/font/google'
+import Image from 'next/image'
 
 const nanum = Nanum_Myeongjo({
   weight: '400',
   subsets: ['latin']
 })
+
+const galleryItems = [{
+  name: 'uofthacksfive1'
+}, {
+  name: 'gotransit1'
+}, {
+  name: 'flora1'
+}, {
+  name: 'uofthacksfive2'
+}, {
+  name: 'flora2'
+}, {
+  name: 'gotransit2'
+}, {
+  name: 'gotransit3'
+}, {
+  name: 'gotransit4'
+}, {
+  name: 'woman1'
+}, {
+  name: 'amacss1'
+}, {
+  name: 'uofthacksfive3'
+}, {
+  name: 'gotransit5'
+}, {
+  name: 'amacss2'
+}, {
+  name: 'amacss3'
+}, {
+  name: 'woman2'
+}, {
+  name: 'flora3'
+}, {
+  name: 'amacss4'
+}, {
+  name: 'amacss5'
+}]
 
 export default function WorkPageComponent() {
   return (
@@ -26,9 +65,19 @@ export default function WorkPageComponent() {
                 was drawn by me.
               </span>
             </div>
+          </div>
+          {galleryItems.map(item => {
+            return (
+              <div className={`halfComponent galleryImage ${item.name}`}>
+              </div>
+            )
+          })}
+          <div className={`fullWidthComponent`}>
             <div className={'textContainer'}>
-              <span className={'header'}>
-                under construction...
+              <span className={'body'}>
+                Thanks for making it to the end. I have a few more art pieces
+                that I'll be posting here soon. When I have time I'll also add
+                some descriptions for each piece, including my process!
               </span>
             </div>
           </div>
